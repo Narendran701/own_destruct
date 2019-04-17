9782233a1083a5b6265a7be13301cce44aaf2d0b
நரேந்திரன் <37191279+Narendran701@users.noreply.github.com>
2019-04-17 16:15:25 +0530

    Update dest.c

--- dest.c
+++ dest.c
@@ -8,27 +8,32 @@ int F_Mask();
 
 void main(int argc, char **argv)
     {
+            FILE *loc = &argv[0];
             char *file_name = argv[0];
-            F_Name(file_name);
+            F_Name(file_name,loc);
     }
 
-    int F_Name(char *file_name)
+    int F_Name(char *file_name,FILE *loc)
         {
             char path[PATH_MAX];
             char cfil[50];
             getcwd(path,sizeof(path));
 	        char *fn = memmove(file_name,file_name+2,strlen(file_name));
             strcat(strcat(strcat(cfil,path),"/"),fn);
-            return F_Mask(cfil);
+            return F_Mask(cfil,loc);
         }
 
-    int F_Mask(char *fullname)
+    int F_Mask(char *fullname,FILE *loc)
         {
-            printf("[> %s <]\n", fullname);
+            short count = 0;
             remove(fullname);
-            FILE *loc;
-            loc = fopen(fullname,"w");
-            fprintf(loc,__TIME__);
-            fclose(loc);            
-           return 0;
+            loc = fopen(fullname,"w+");            
+            while(count<5)
+                {
+                    fprintf(loc,"The time is [ %s ]\n",__TIME__);
+                    printf("NOW [ %d ]\n",__TIME__);
+                    count++;
+                }
+            fclose(loc);
+            return 0;  
         }
